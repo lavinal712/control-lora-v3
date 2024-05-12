f85a36bdd315d3ae9f534ce7ad134c1e8d5aed10
Yuqian Hong <austin712@mail.ustc.edu.cn>
2024-05-13 05:21:40 +0800

    Update merge_lora.py

--- merge_lora.py
+++ merge_lora.py
@@ -1,30 +1,18 @@
-import os
-import sys
-import time
-from pathlib import Path
-
-import numpy as np
 import torch
-import torch.utils.checkpoint
-from accelerate import Accelerator
-from diffusers import DDIMScheduler, ControlNetModel, UNet2DConditionModel
-from diffusers.utils import check_min_version
-from safetensors.torch import load_file
-from tqdm import tqdm
-from transformers import AutoTokenizer
-from PIL import Image
 
+import diffusers
+from diffusers import UNet2DConditionModel, ControlNetModel
 from peft import PeftModel
-from control_lora_pipeline import LightControlNetPipeline
 
 
 base_model = "F:/stable-diffusion-v1-5"
+controlnet_save_path = "./save"
 
 unet = UNet2DConditionModel.from_pretrained(
     base_model, subfolder="unet", torch_dtype=torch.float16
 )
-controlnet = ControlNetModel.from_unet(unet)
 
-control_lora = PeftModel.from_pretrained(controlnet, "control-lora")
-control_lora = control_lora.merge_and_unload()
-control_lora.save_pretrained("./save")
+controlnet = ControlNetModel.from_unet(unet)
+controlnet = PeftModel.from_pretrained(controlnet, "controlnet")
+controlnet = controlnet.merge_and_unload()
+controlnet.save_pretrained(controlnet_save_path)
