b4c946f3a5b2a203f7f5131758048db25721a0e4
Yuqian Hong <austin712@mail.ustc.edu.cn>
2024-05-15 07:38:16 +0800

    Update merge_lora.py

--- merge_lora.py
+++ merge_lora.py
@@ -5,8 +5,8 @@ from diffusers import UNet2DConditionModel, ControlNetModel
 from peft import PeftModel
 
 
-base_model = "F:/stable-diffusion-v1-5"
-controlnet_save_path = "./save"
+base_model = "runwayml/stable-diffusion-v1-5"
+controlnet_save_path = "save/path"
 
 unet = UNet2DConditionModel.from_pretrained(
     base_model, subfolder="unet", torch_dtype=torch.float16
@@ -14,5 +14,5 @@ unet = UNet2DConditionModel.from_pretrained(
 
 controlnet = ControlNetModel.from_unet(unet)
 controlnet = PeftModel.from_pretrained(controlnet, "controlnet")
-controlnet = controlnet.merge_and_unload()
-controlnet.save_pretrained(controlnet_save_path)
+merged_controlnet = controlnet.merge_and_unload()
+merged_controlnet.save_pretrained(controlnet_save_path)
